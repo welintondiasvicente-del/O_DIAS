410a0bba3c57ed57cb5cbe4b175bc77b0f7885ea
O DIAS <welintondiasvicente@gmail.com>
2026-08-30 09:24:50 -0300

    Refactor service worker caching logic
    Updated cache name and improved fetch handling.

--- sw.js
+++ sw.js
@@ -1,17 +1,30 @@
-const CACHE_NAME = 'o-dias-v1';
+const CACHE_NAME = 'o-dias-cache';
 const urlsToCache = [
   './',
   './index.html'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
